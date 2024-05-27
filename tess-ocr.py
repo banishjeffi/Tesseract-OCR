215cdcf8524deccd8a6da957f992c867bcfb57ff
Banish J <166292800+banishjeffi@users.noreply.github.com>
2024-05-27 10:35:14 +0530

    Add files via upload

---
+++ tess-ocr.py
@@ -0,0 +1,19 @@
+try:
+    from PIL import Image
+except ImportError:
+    import Image
+
+import pytesseract
+
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+def recText (filename):
+    text = pytesseract.image_to_string (Image.open(filename))
+    return text
+
+info = recText('TEST6.png')
+print (info)
+
+file = open("New.txt","a")
+file.write (info)
+file.close()
+print("Written Successful")
